365d9481b28e28765e033de590c38c643fac9148
Aidan Fu <aidan.fu000@gmail.com>
2023-01-03 01:19:38 -0500

    move useAuth into lifecycle hook

--- src/hooks/lifecycles/useAppLifecycle.tsx
+++ src/hooks/lifecycles/useAppLifecycle.tsx
@@ -1,8 +1,10 @@
 import useMobileTheme from './useMobileTheme';
 import { useUserStatusSocket } from './sockets/useUserStatusSocket';
 import { useSocketConnection } from './sockets/useSocketConnection';
+import { useAuth } from './useAuth';
 
 export const useAppLifecycle = () => {
+  useAuth();
   useMobileTheme();
   useSocketConnection();
   useUserStatusSocket();
