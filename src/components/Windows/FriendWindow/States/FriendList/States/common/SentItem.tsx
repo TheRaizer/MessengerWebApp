e22e311ad19030ace7ea44c8544426f206853791
Aidan Fu <aidan.fu000@gmail.com>
2023-01-26 17:19:46 -0500

    add general component for sent and recieved friend items

--- src/components/Windows/FriendWindow/States/FriendList/States/common/SentItem.tsx
+++ src/components/Windows/FriendWindow/States/FriendList/States/common/SentItem.tsx
@@ -1,46 +1,15 @@
-import { ReactElement, useState } from 'react';
-import { FriendInfo } from './FriendInfo';
+import { ReactElement } from 'react';
 import { FriendItemProps } from '../../../../../../../../types/components/Windows/FriendWindow/States/FriendList/common/FriendItem.type';
-import { ItemButton } from './ItemPillButton';
-import { fetchNextAPI } from '../../../../../../../helpers/api/api';
-import { Spinner } from '../../../../../../Loading/Spinner';
+import { AddressRequestItem } from './AddressRequestItem';
 
-export const SentItem = ({
-  friendUsername,
-  mutate,
-}: FriendItemProps): ReactElement => {
-  const [loading, setLoading] = useState(false);
-
+export const SentItem = (props: FriendItemProps): ReactElement => {
   return (
-    <FriendInfo friendUsername={friendUsername}>
-      {loading ? (
-        <Spinner color="black" size={1} />
-      ) : (
-        <ItemButton
-          onClick={() => {
-            setLoading(true);
-
-            fetchNextAPI(
-              `friends/requests/cancel?request_addressee_username=${friendUsername}`,
-              'POST'
-            ).catch((err) => console.error(err));
-
-            mutate((data) => {
-              data?.forEach((cursorModel) => {
-                cursorModel.results = cursorModel.results.filter(
-                  (userModel) => userModel.username !== friendUsername
-                );
-              });
-
-              return data;
-            })
-              .catch((err) => console.error(err))
-              .finally(() => setLoading(false));
-          }}
-        >
-          Cancel
-        </ItemButton>
-      )}
-    </FriendInfo>
+    <AddressRequestItem
+      {...props}
+      getRoute={(friendUsername) =>
+        `friends/requests/cancel?request_addressee_username=${friendUsername}`
+      }
+      buttonText={'Cancel'}
+    />
   );
 };
