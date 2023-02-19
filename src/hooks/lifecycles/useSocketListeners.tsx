bda4967adf881f20a38b6969816fba2031f6e725
Aidan Fu <aidan.fu000@gmail.com>
2023-02-18 20:20:41 -0500

    add message socketio events

--- src/hooks/lifecycles/useSocketListeners.tsx
+++ src/hooks/lifecycles/useSocketListeners.tsx
@@ -1,7 +1,9 @@
+import { useMessageSocket } from './sockets/useMessageSocket';
 import { useSocketConnection } from './sockets/useSocketConnection';
 import { useUserStatusSocket } from './sockets/useUserStatusSocket';
 
 export const useSocketListeners = () => {
   useSocketConnection();
   useUserStatusSocket();
+  useMessageSocket();
 };
